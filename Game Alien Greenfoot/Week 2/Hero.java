a44962c891f1522d1101a22996345e3fb21e5a0d
losinanda <kadekprawira29@gmail.com>
2022-10-05 22:27:44 +0800

    Upload Progress Week 2

---
+++ Game Alien Greenfoot/Week 2/Hero.java
@@ -0,0 +1,35 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        moveHero();
+    }
+    
+    public void moveHero() {
+        if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 5);
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 5);
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setLocation(getX() - 5, getY());
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            setLocation(getX() + 5, getY());
+        }        
+    }
+}
