3b529da8ae08a9afdd9a3ceb76d49ce171685193
losinanda <kadekprawira29@gmail.com>
2022-10-11 17:47:41 +0800

    Upload Progress Week 3

---
+++ Game Alien Greenfoot/Week 3/Hero.java
@@ -0,0 +1,47 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Hero here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hero extends Actor
+{
+    /**
+     * Act - do whatever the Hero wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        moveHero();
+        shootLaser();
+    }
+    
+    public void moveHero() {
+        if (Greenfoot.isKeyDown("w")) {
+            setLocation(getX(), getY() - 5);
+        }
+        if (Greenfoot.isKeyDown("s")) {
+            setLocation(getX(), getY() + 5);
+        }
+        if (Greenfoot.isKeyDown("a")) {
+            setLocation(getX() - 5, getY());
+        }
+        if (Greenfoot.isKeyDown("d")) {
+            setLocation(getX() + 5, getY());
+        }        
+    }
+    
+    int laserTimer = 0;
+    public void shootLaser() {
+        if (laserTimer == 30) {
+            getWorld().addObject(new Laser(), getX() + 70, getY());
+            laserTimer = 0;
+        }
+        else {
+            laserTimer++;
+        }
+    }
+}
---
+++ Game Alien Greenfoot/Week 3/MyWorld.java
@@ -0,0 +1,39 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class MyWorld here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class MyWorld extends World
+{
+    /**
+     * Constructor for objects of class MyWorld.
+     * 
+     */
+    public MyWorld()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+        prepare();
+    }
+    private void prepare() {
+        Hero hero = new Hero();
+        addObject(hero,100,300);
+    }
+    public void act () {
+        spawnEnemy();
+    }
+    public int timerEnemy = 0;
+    public void spawnEnemy() {
+        if (timerEnemy == 180) { // if the timer is 3 second
+            int speed = Greenfoot.getRandomNumber(4) + 1;
+            addObject(new Enemy(), 599, Greenfoot.getRandomNumber(400));
+            timerEnemy = 0;
+        }
+        else {
+            timerEnemy++;
+        }
+    }
+}
